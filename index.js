c6e781479fb1829157f3d1b282829fdf43c71354
jayanta kumar sahoo <sjayantakumar20@gmail.com>
2021-05-01 12:51:02 +0530

    issue resolved

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
-const path = require('path');
+const express = require("express");
+const exphbs = require("express-handlebars");
+const path = require("path");
 const port = 5000;
 const app = express();
-app.use(express.static('public'));
-app.engine('hbs' , exphbs({ defaultLayout: 'index' , extname: 'hbs'}));
-app.set('view engine' , 'hbs');
+app.use(express.static("public"));
+app.engine("hbs", exphbs({ defaultLayout: "index", extname: "hbs" }));
+app.set("view engine", "hbs");
 
-app.get('/',(req,res)=>{
-    res.render('home',{layout:'main'})
-})
-app.get('/hoverboard', (req,res) =>{
-
-    res.render('Hoverboard');
+app.get("/", (req, res) => {
+  res.render("home", { layout: "main" });
 });
-app.get('/TicTacToe',(req,res)=>{
-    res.sendFile(path.join(__dirname +'/ticTactoe.html'));
+app.get("/hoverboard", (req, res) => {
+  res.render("Hoverboard");
+});
+app.get("/TicTacToe", (req, res) => {
+  res.sendFile(path.join(__dirname + "/ticTactoe.html"));
 });
 
-app.listen(process.env.PORT || port, ()=>{
-    console.log("server started....");
+app.listen(process.env.PORT || port, () => {
+  console.log("server started....");
 });
